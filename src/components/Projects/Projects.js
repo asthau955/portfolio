286b998187f19418e4e55b673919d23007a046a4
Astha Upadhyay <asthaupadhyay955@gmail.com>
2023-01-31 09:37:42 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -35,7 +35,7 @@ function Projects() {
               title="Bits-0f-C0de"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               ghLink="https://github.com/asthau955/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
@@ -46,7 +46,7 @@ function Projects() {
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink="https://github.com/asthau955/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              demoLink="https://chatify-49.web.app/"
             />
           </Col>
         </Row>
